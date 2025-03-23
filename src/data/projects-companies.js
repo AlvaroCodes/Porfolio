beb08091021e57f24534f467f42b315a64690387
AlvaroCodes <aramirezm098@gmail.com>
2025-03-23 10:53:51 +0100

    Primera subida

---
+++ src/data/projects-companies.js
@@ -0,0 +1,102 @@
+// CONTENT_PROJECT_1
+import CKAN from "../assets/ckan.webp";
+import COMPARADOR from "../assets/comparador.webp";
+import API_CNIG from "../assets/apicnig.webp";
+import API_IDEE from "../assets/apiidee.webp";
+import ESTEREO from "../assets/estereoscopico.webp";
+import NOMENCLATOR from "../assets/nomenclator.webp";
+import MADRID_TEXEIRA from "../assets/madrid-texeira.webp";
+
+// CONTENT_PROJECT_2
+import IBERPIX from "../assets/iberpix.webp";
+import TERREMOTOS from "../assets/terremotos.webp";
+import REDES_GEODESICAS from "../assets/redes_geodesicas.webp";
+import COMPARADOR_PNOA from "../assets/comparador_pnoa.webp";
+
+import ACELEROGRAFOS from "../assets/acelerografos.webp";
+import CARTOCIUDAD from "../assets/cartociudad.webp";
+import ESTACIONES_SISMICAS from "../assets/estaciones_sismicas.webp";
+import PLANOS_ANTIGUOS from "../assets/planosAntiguos.webp";
+
+
+export const CONTENT_PROJECT_1 = [
+  {
+    title: "API-CNIG",
+    url: '/projects/api-cnig',
+    image: API_CNIG,
+  },
+  {
+    title: "API-IDEE",
+    url: '/projects/api-idee',
+    image: API_IDEE,
+  },
+  {
+    title: "Comparador de Mapas",
+    url: '/projects/comparadormapas',
+    image: COMPARADOR,
+  },
+  {
+    title: "Portal de datos abiertos del Ayuntamiento de Madrid",
+    url: '/projects/ckan',
+    image: CKAN,
+  },
+  {
+    title: "Nomenclator",
+    url: '/projects/ngn-ngbe',
+    image: NOMENCLATOR,
+  },
+  {
+    title: "Visor Estereoscópico",
+    url: '/projects/estereoscopico',
+    image: ESTEREO,
+  },
+  {
+    title: "Madrid Texeira",
+    url: '/projects/madrid-texeira',
+    image: MADRID_TEXEIRA,
+  },
+  {
+    title: "Redes Geodésicas",
+    url: '/projects/redes-geodesicas',
+    image: REDES_GEODESICAS,
+  }
+];
+
+
+export const CONTENT_PROJECT_2 = [
+  {
+    title: "Iberpix",
+    url: '/projects/iberpix',
+    image: IBERPIX,
+  },
+  {
+    title: "Terremotos",
+    url: '/projects/terremotos',
+    image: TERREMOTOS,
+  },
+  {
+    title: "Comparador PNOA",
+    url: '/projects/comparador-pnoa',
+    image: COMPARADOR_PNOA,
+  },
+  {
+    title: "Acelerógrafos",
+    url: '/projects/acelerografos',
+    image: ACELEROGRAFOS,
+  },
+  {
+    title: "Cartociudad",
+    url: '/projects/cartociudad',
+    image: CARTOCIUDAD,
+  },
+  {
+    title: "Estaciones Sísmicas",
+    url: '/projects/estaciones-sismicas',
+    image: ESTACIONES_SISMICAS,
+  },
+  {
+    title: "Planos Antiguos",
+    url: '/projects/planos-antiguos',
+    image: PLANOS_ANTIGUOS,
+  }
+];
---
+++ src/data/projects-freelance.js
@@ -0,0 +1,28 @@
+//CONTETN_PROJECT_FREELANCE
+import BASICA from "../assets/basica.webp";
+import EGORF from "../assets/egorf.webp";
+
+// CONTENT_MY_PROJECTS
+import APRENDE_OL from "../assets/aprende-ol.webp";
+
+
+export const CONTENT_PROJECT_FREELANCE = [ 
+    {
+        title: "Básica",
+        url: '/projects/basica',
+        image: BASICA,
+    },
+    {
+        title: "Egorf",
+        url: '/projects/egorf',
+        image: EGORF,
+    }
+];
+
+export const CONTENT_MY_PROJECTS = [
+    {
+        title: "Aprende Openlayers",
+        url: '/projects/aprende-ol',
+        image: APRENDE_OL,
+    }
+];
