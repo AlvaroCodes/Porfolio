5fe34811ebe84d8968557ca8c9d47c8f98ceec53
AlvaroCodes <aramirezm098@gmail.com>
2025-03-23 12:04:59 +0100

    Se elimina pages, pasa netlify

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,4 @@ import mdx from '@astrojs/mdx';
 // https://astro.build/config
 export default defineConfig({
   integrations: [mdx()],
-  site: 'https://alvarocodes.github.io',
-  base: 'porfolio',
 });
